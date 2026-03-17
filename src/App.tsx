ae649a74be662472d3a53338fda7b980b2f55805
sothearn1th <sothearnith1@gmail.com>
2026-03-17 08:29:27 -0400

    Understanding layout
    <div> <header> <button> etc

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,17 @@ export default function App()
     <div>
       <h1>Hello world</h1>
       <button onClick={handleClick}>Click me</button>
+        <div> 
+          <h1>Another section</h1>
+          <button onClick={handleClick}>Click me too</button>
+        </div>
+
+        <div> 
+          <h1>Yet another section</h1>
+          <button onClick={handleClick}>Click me three</button>
+        </div>
     </div>
+
+
   );
 }
