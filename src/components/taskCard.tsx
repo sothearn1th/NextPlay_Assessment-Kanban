82fa29068a97e7e6f33ebf0e1d894ee118f21a39
sothearn1th <sothearnith1@gmail.com>
2026-03-17 18:42:27 -0400

    Added some font
    Old Vario for the header and title
    ClassVariable for the description

--- src/components/taskCard.tsx
+++ src/components/taskCard.tsx
@@ -13,12 +13,15 @@ export function TaskCard({ task, onDelete }: TaskCardProps)
       style={{
         border: "1px solid gray",
         borderRadius: "12px",     // rounded edges
+        backgroundColor: "#3f3f3f", // A slightly lighter dark grey for the card background
         padding: 10,
         marginBottom: 8
       }}
     >
+
+    
       <p>{task.title}</p>
-      <p style={{ fontSize: 12, color: "gray" }}>
+      <p style={{font: "status-bar", fontSize: 12, fontWeight: "bold", color: "gray" }}>
         {task.created_at.toLocaleString()}
       </p>
       <button onClick={() => onDelete(task.id)}>Delete Task</button>
